12f14221414ba6527e7e9cda8c69fd156d3021c1
Prathmesh Wasade <74913070+prathu123456@users.noreply.github.com>
2021-06-13 15:33:29 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,41 @@
+Hours = "thirty"
+print(Hours)
+
+Days = "Thirty days"
+print(Days[0])
+
+Challenge = "I will win"
+print(Challenge[7:10])
+
+Challenge = "I will win"
+print(len(Challenge))
+
+Challenge = "I will win"
+print(Challenge.lower())
+
+a = "30 Days"
+b = "30 hours"
+c = a + b
+print(c)
+
+a = "30 Days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+text1 = "DON’T TROUBLE TROUBLE UNTIL TROUBLE TROUBLES YOU"
+x = text1.capitalize()
+print(x)
+
+x = text1.find(text)
+print(x)
+
+x = text1.isalpha()
+print(x)
+
+x = text1.isalnum()
+print(x)
