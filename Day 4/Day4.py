aa6537edb51edeb92fde1331595f2056834fee59
Prathmesh Wasade <74913070+prathu123456@users.noreply.github.com>
2021-06-18 02:09:55 +0530

    Add files via upload

---
+++ Day 4/Day4.py
@@ -0,0 +1,19 @@
+#1)Create three variables (a,b,c) to same value of any integer & do the following
+#a) Divide a by 10
+#b) Multiply b by 50
+#c) Add c value by 60
+a = b = c = 10
+print(a/10)
+print(b*50)
+print(c+60)
+
+#2)Create a String variable of 5 characters and replace the 3rd character with G
+a = 'ALRAE'
+a = a.replace('R','G')
+print(a)
+
+#3)Create two values (a,b) of int,float data type & convert the vise versa,
+#Hint : convert a from int to float datatype & b from float to int datatype
+a,b = 20,30.35
+print(float(a))
+print(int(b))
